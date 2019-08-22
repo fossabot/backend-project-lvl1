6c0914fee007b3fb4952d4166eeb0e7d22401c68
Artyom Perederiy <neysrow@gmail.com>
2019-08-22 20:11:44 +0300

    stage 7

---
+++ src/bin/brain-gcd.js
@@ -0,0 +1,3 @@
+import initialGame from '../games/brain-gcd';
+
+initialGame();
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,19 @@
+import start, { getRndNumInRange, cons } from '..';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (firstNum, secondNum) => {
+  if (firstNum > secondNum) return getGcd(secondNum, firstNum);
+  if (!secondNum) return `${firstNum}`;
+  return getGcd(secondNum, secondNum % firstNum);
+}
+
+const gameData = () => {
+  const firstNum = getRndNumInRange(0, 11);
+  const secondNum = getRndNumInRange(0, 11);
+  const question = `${firstNum} ${secondNum}`;
+  const rightAnswer = getGcd(firstNum, secondNum);
+  return cons(question, rightAnswer);
+};
+
+export default () => start(rules, gameData);
