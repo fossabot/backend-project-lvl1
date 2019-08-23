dd89532f0ba9313b62fd3a0d6cc85c00ddc8ac45
Artyom Perederiy <neysrow@gmail.com>
2019-08-23 15:22:53 +0300

    fix ENV path

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node;
+#!/usr/bin/env node
 
 import initialGame from '../games/brain-calc';
 
--- src/bin/brain-prime.js
+++ src/bin/brain-prime.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import initialGame from '../games/brain-prime';
 
 initialGame();
